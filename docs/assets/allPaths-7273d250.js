e1228828c10a41467fa7890d208aabd5428b7a06
bofa <per-erik.karlsson@tietoevry.com>
2024-08-14 11:08:56 +0200

    build

---
+++ docs/assets/allPaths-7273d250.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-185eca5c.js";import{I as e}from"./index-70b50411.js";import{p as r,I as s}from"./index-b437bfdc.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
