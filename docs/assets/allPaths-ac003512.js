51e7dc3bb291888b5a412df04cad87780ffbb5d0
Per-Erik Karlsson <per-erik.karlsson@tietoevry.com>
2024-12-12 09:50:03 +0100

    build

---
+++ docs/assets/allPaths-ac003512.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-6d17fd53.js";import{I as e}from"./index-183a7b3c.js";import{p as r,I as s}from"./index-e40a6e2c.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
