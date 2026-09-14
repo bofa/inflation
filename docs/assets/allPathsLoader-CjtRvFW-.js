3c5619692ce28724ff6b34cd1fa192cf785587f3
Per-Erik Karlsson <per-erik.karlsson@tietoevry.com>
2026-09-14 15:34:21 +0200

    build

---
+++ docs/assets/allPathsLoader-CjtRvFW-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./allPaths-DVeTdEeW.js","./index-Bck4KEe_.js","./index-W58HgMhY.js","./index-Z5l7Ucrx.js","./index-C--toWVm.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-Z5l7Ucrx.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-DVeTdEeW.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]),import.meta.url);return o(t,a)};export{s as allPathsLoader};
